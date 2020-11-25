4825256d4c43946c28eb98bbea650b294323cdd2
Aravind V V <arvndvv@gmail.com>
2020-11-25 22:10:52 +0530

    awesome patterns

--- one/script.js
+++ one/script.js
@@ -4,22 +4,21 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let number = 0;
-let scale = 10;
+let number = 20; //tweak this number to get different center shape
+let scale = 1; // can be used to increase gap
 
 
 function drawFlower() {
-    let angle = number * 1;
-
+    let angle = number * 20; //play with 10 here to get different patterns
     let radius = scale * Math.sqrt(number);
-    let posX = radius * Math.sin(angle);
-    let posY = radius * Math.cos(angle);
-    ctx.fillStyle = "red";
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "blue";
+    let posX = radius * Math.sin(angle) + canvas.width / 2;
+    let posY = radius * Math.cos(angle) + canvas.height / 2;
+    ctx.fillStyle = "#eb3f4a";
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#febcda";
     ctx.beginPath();
 
-    ctx.arc(posX, posY, 10, 0, Math.PI * 2);
+    ctx.arc(posX, posY, 20, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
