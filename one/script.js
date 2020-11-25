1b95c431661e5392fdf3f31c9089ae4163b2515e
Aravind V V <arvndvv@gmail.com>
2020-11-25 18:51:04 +0530

    loader

--- one/script.js
+++ one/script.js
@@ -4,9 +4,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let size = 0;
-let posX = 0;
-let posY = 0;
+let posX = canvas.width / 2;
+let posY = canvas.height / 2;
 let angle = 0;
 
 function drawFlower() {
@@ -14,7 +13,8 @@ function drawFlower() {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "blue";
     ctx.beginPath();
-    ctx.arc(posX, posY, size, 0, Math.PI * 2);
+
+    ctx.arc(posX, posY, 10, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
@@ -22,11 +22,11 @@ function drawFlower() {
 
 function animate() {
     //we need to clear canvas each time
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    size += 0.05;
-    posX += 0.5;
-    posY += 0.5;
-    angle += 0.1;
+    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    posX += 2 * Math.sin(angle);
+    posY += 2 * Math.cos(angle);
+    angle += 0.05;
     drawFlower();
     requestAnimationFrame(animate);
 }
